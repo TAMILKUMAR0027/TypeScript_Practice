fd04d515e62c6f5f46296fb3729f5b43f1c3b0ae
TAMILKUMAR0027 <tamilkumar0027@gmail.com>
2026-06-22 14:33:11 +0530

    Folder Added

---
+++ Practice_Problems/GlobalScope.ts
@@ -0,0 +1,5 @@
+let globalVar: string="im a global"
+function exampleFun(): void{
+    console.log(globalVar)
+}
+exampleFun()
---
+++ Practice_Problems/Union.ts
@@ -0,0 +1,5 @@
+let value:number|string;
+value=1120
+console.log(value)
+value="i"
+console.log(value)
